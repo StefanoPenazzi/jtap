5724114a6d71ad72ec586b0a7bc2e78600ee5bd1
Stefano Penazzi <ste.penazzi1987@gmail.com>
2021-05-28 02:27:50 -0700

    logger

--- src/main/java/data/external/neo4j/Neo4jConnection.java
+++ src/main/java/data/external/neo4j/Neo4jConnection.java
@@ -1,11 +1,9 @@
 package data.external.neo4j;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.neo4j.driver.*;
-
-import com.google.inject.Inject;
-
-import static org.neo4j.driver.Values.parameters;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -14,12 +12,12 @@ import java.util.Properties;
 
 public class Neo4jConnection implements AutoCloseable {
 	
+	private static Logger logger = LogManager.getLogger(Neo4jConnection.class);
 	private final Driver driver;
 	private final String uri;
 	private final String username;
 	private final String password;
 	
-	@Inject
 	public Neo4jConnection()
     {
 		Properties properties = new Properties();
@@ -33,6 +31,7 @@ public class Neo4jConnection implements AutoCloseable {
 	    this.username = properties.getProperty("neo4j_username");
 	    this.password = properties.getProperty("neo4j_password");
         driver = GraphDatabase.driver( uri, AuthTokens.basic( username, password ) );
+        logger.log(Level.INFO," Neo4j Driver On ") ;
     }
 	
 	public Driver getDriver() {
@@ -43,6 +42,7 @@ public class Neo4jConnection implements AutoCloseable {
     public void close() throws Exception
     {
         driver.close();
+        logger.log(Level.INFO," Neo4j Driver Off ") ;
     }
 
 }
