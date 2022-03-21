1f3f1a9cb1589789250835cdf457338bdda167cd
Stefano Penazzi <ste.penazzi1987@gmail.com>
2022-03-21 03:40:13 -0700

    no agent test

--- src/test/java/projects/CTAP/model/AgentTest.java
+++ src/test/java/projects/CTAP/model/AgentTest.java
@@ -13,21 +13,7 @@ class AgentTest {
 
 	@Test
 	void test() {
-		Config config = Config.of(Paths.get("/home/stefanopenazzi/projects/jtap/config_.xml").toFile()); 
-		Controller controller = new Controller(config);
-		controller.run();
-		controller.emptyTempDirectory();
-		PopulationFactory populationFactory = controller.getInjector().getInstance(PopulationFactory.class);
-		Population population = populationFactory.run();
-		Agent ag = (Agent) population.getAgentsIterator().next();
 		
-		double[] initialGuess = new double[] {0,2000,4000,5000,6000,7000,10000,15000,20000,22000,1999,3999,4999,5999,6999,7999,14999,19999,21999,22999};
-		SolverImpl si = new SolverImpl.Builder((ag.getAgentModels().get(0)))
-				.initialGuess(initialGuess)
-				.build();
-		si.run();
-		
-		System.out.println();
 	    
 	}
 
